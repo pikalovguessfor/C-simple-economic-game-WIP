14c865fbd04e6a7a8a9d6f98e27bbeef3d3484da
pikalovvova1303 <pikalovvova1303@gmail.com>
2024-07-21 22:59:51 +0300

    v 0.1.1 realized "ChangeSituatStab" function

--- gameEco/Events.h
+++ gameEco/Events.h
@@ -5,21 +5,37 @@
 #include "includes.h"
 
 namespace gameEvents {																				//this file have own namespace for make code more EXPANSIONAL
+		
+	// this is main arrays to work with events
+	const short arrSitAllOk[9] =																	// Const array to comparison dynamic array "ArrSituation" on best way
+	{
+	1, 1, 1,
+	1, 1, 1,
+	1, 1, 1,
+	};
 
-	short ArrSituation[9] =																			// In this array count 1 this is "Stability" number, 0 this is "unstability numbers"
+	const short arrSitAllBad[9] =																	// Const array to comparison dynamic array "ArrSituation" on bad way
+	{
+		0, 0, 0,
+		0, 0, 0,
+		0, 0, 0
+	};
+
+	short ArrSituationDyn[9] =																		// In this array count 1 this is "Stability" number, 0 this is "unstability numbers"
 	{
 		1, 1, 1,
 		1, 1, 1,
-		1, 1, 1,
+		1, 1, 1
 	};
+	//
 
 	bool MinusCh() {																				// Event for chech array "ArrSituation[9]" have more three negative counts
 
-		short countMinus;
+		short countMinus = 0;
 
 		for (int i = 0; i <= 9; i++) {
 
-			if (ArrSituation[i] == 0) {
+			if (ArrSituationDyn[i] == 0) {
 
 				countMinus++;
 
@@ -37,21 +53,47 @@ namespace gameEvents {																				//this file have own namespace for mak
 
 	}
 
-	void ChageSituatStab() {																		// Event for change numbers in array "ArrSituation[9]"
+	void ChangeSituatStab() {																		// Event for change numbers in array "ArrSituation[9]"
+		srand(time(0));
 
+		short arrInsideSit[] =
+		{
+		1, 1, 1,
+		1, 1, 1,
+		1, 1, 1
+		};
 
+		bool allRolled = false;
+		
+		short RollArr[9];
+
+		while (!allRolled) {
+			
+			for (int i = 0; i <= 9; i++) {
+
+				RollArr[i] = (rand() % 200);
+
+				if (RollArr[i] >= 100) {
+					arrInsideSit[i] = 0;
+				}
+				else {
+					
+					arrInsideSit[i] = 1;
+
+				}
+
+			}
+			
+			allRolled = true;
+
+		}
 
 	}
 
+
 	void eventDivIncome(float income) {																// Event to Division "income" of actor
 
-		srand(time(0));
-
-		if (MinusCh) {
-
-
-
-		}
+		
 
 
 		return;
